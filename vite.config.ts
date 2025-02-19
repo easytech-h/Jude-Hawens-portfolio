37badc6d7120860ea614182fe25e26d564065ab7
easytech-h <fofovictorflo@gmail.com>
2025-02-18 23:26:01 -0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Jude-Hawens-portfolio/',  // Ajoute cette ligne
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
